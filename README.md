e1948320dffce93e3d751d7392f23da49c1e921f
💖 Chloe 💖 <gaycoderprincess@proton.me>
2025-01-02 15:08:37 +0100

    nya :3

---
+++ README.md
@@ -0,0 +1,19 @@
+# FlatOut 2 In-Game DB Editor
+
+Plugin to modify the game database in FlatOut 2 in real time
+
+![preview image](https://i.imgur.com/Fg8fHZv.png)
+
+## Installation
+
+- Make sure you have v1.2 of the game, as this is the only version this plugin is compatible with. (exe size of 2990080 bytes)
+- Plop the files into your game folder, press F5 in game to open the editor menu.
+- Enjoy, nya~ :3
+
+## Building
+
+Building is done on an Arch Linux system with CLion being used for the build process. 
+
+Before you begin, clone [nya-common](https://github.com/gaycoderprincess/nya-common) and [nya-common-fouc](https://github.com/gaycoderprincess/nya-common-fouc) to folders next to this one, so it can be found.
+
+You should be able to build the project now in CLion.
---
+++ main.cpp
@@ -0,0 +1,33 @@
+#include <windows.h>
+#include <d3d9.h>
+#include <format>
+#include "toml++/toml.hpp"
+
+#include "nya_commonmath.h"
+#include "nya_commonhooklib.h"
+
+#include "fo2.h"
+#include "../nya-common-fouc/fo2versioncheck.h"
+#include "chloemenulib.h"
+
+uint32_t nodeVtable = 0x67B1C0;
+void DBEditor_PatchOnApply() {
+	NyaHookLib::Patch(0x558695 + 1, "PropertyDb: Table '%s' not found!");
+	NyaHookLib::Patch<uint32_t>(0x4ED804, 0);
+	NyaHookLib::Patch<uint32_t>(0x4EDB51, 0);
+	NyaHookLib::Fill(0x675268, 0x90, 0x675294-0x675268);
+}
+
+#include "../FlatOutUCIngameDBEditor/dbeditor.h"
+
+BOOL WINAPI DllMain(HINSTANCE, DWORD fdwReason, LPVOID) {
+	switch( fdwReason ) {
+		case DLL_PROCESS_ATTACH: {
+			DoFlatOutVersionCheck(FO2Version::FO2_1_2);
+			ChloeMenuLib::RegisterMenu("In-Game DB Editor - gaycoderprincess", DBEditorLoop);
+		} break;
+		default:
+			break;
+	}
+	return TRUE;
+}
